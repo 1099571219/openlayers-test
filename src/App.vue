41b403cae07c1f316a028a812ab648f81e9bd144
ZK <1099571219@qq.com>
2023-11-01 22:19:45 +0800

    reset: 添加操作提示文本

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 // import OSM from 'ol/source/OSM'    // OSM瓦片【OSM不能在实际开发中使用，因为OSM里中国地图的边界有点问题！！！！】
 // import 'ol/ol.css'                 // 地图样式
 
+import 'ol/ol.css';
 import Draw from 'ol/interaction/Draw';
 import Map from 'ol/Map';
 import Overlay from 'ol/Overlay';
@@ -15,24 +16,53 @@ import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 import { getArea, getLength } from 'ol/sphere';
 import { unByKey } from 'ol/Observable';
 import XYZ from 'ol/source/XYZ'
-
 import { ref, onMounted } from 'vue'
 const mapElement = ref()
 onMounted(() => {
   // const raster = new TileLayer({
   //   source: new OSM(),
   // });
-  var raster = new TileLayer({
-    title: "高德地图",
-    source: new XYZ({
-      url: 'http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}',
-      wrapX: false
+  initMap()
+  distance()
+})
+
+
+function initMap() {
+  // 地图实例
+  map = new Map({
+    target: mapElement.value,                         // 对应页面里 id 为 map 的元素
+    layers: [                              // 图层
+      raster
+    ],
+    view: new View({                       // 地图视图
+      projection: "EPSG:4326",             // 坐标系，有EPSG:4326和EPSG:3857
+      center: [114.064839, 22.548857],     // 深圳坐标
+      minZoom: 5,                          // 地图缩放最小级别
+      zoom: 15                             // 地图缩放级别（打开页面时默认级别）
     })
-  });
+  })
+}
 
-  const source = new VectorSource();
+let raster = new TileLayer({
+  title: "高德地图",
+  source: new XYZ({
+    url: 'http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}',
+    wrapX: false
+  })
+});
+let map = null
+let helpTooltipElement = null
+let feature = null;
+let helpTooltip = null;
+let draw = null;
+let measureTooltipElement = null;
+let measureTooltip = null;
+let listener = null;
+let mapMouseMove = null;
 
-  const vector = new VectorLayer({
+function distance() {
+  let source = new VectorSource()  // 首先创建一个数据源，用来放置绘制过程中和绘制结束后的线段
+  const layer = new VectorLayer({  // 添加一个图层，用来放置数据源，样式自己随便设置就可以了，我这里默认的官网
     source: source,
     style: new Style({
       fill: new Fill({
@@ -40,7 +70,7 @@ onMounted(() => {
       }),
       stroke: new Stroke({
         color: '#ffcc33',
-        width: 2,
+        width: 4,
       }),
       image: new CircleStyle({
         radius: 7,
@@ -50,264 +80,40 @@ onMounted(() => {
       }),
     }),
   });
-
-  /**
-   * Currently drawn feature.
-   * @type {import("../src/ol/Feature.js").default}
-   */
-  let sketch;
-
-  /**
-   * The help tooltip element.
-   * @type {HTMLElement}
-   */
-  let helpTooltipElement;
-
-  /**
-   * Overlay to show the help messages.
-   * @type {Overlay}
-   */
-  let helpTooltip;
-
-  /**
-   * The measure tooltip element.
-   * @type {HTMLElement}
-   */
-  let measureTooltipElement;
-
-  /**
-   * Overlay to show the measurement.
-   * @type {Overlay}
-   */
-  let measureTooltip;
-
-  /**
-   * Message to show when the user is drawing a polygon.
-   * @type {string}
-   */
-  const continuePolygonMsg = 'Click to continue drawing the polygon';
-
-  /**
-   * Message to show when the user is drawing a line.
-   * @type {string}
-   */
-  const continueLineMsg = 'Click to continue drawing the line';
-
-  /**
-   * Handle pointer move.
-   * @param {import("../src/ol/MapBrowserEvent").default} evt The event.
-   */
-  const pointerMoveHandler = function (evt) {
-    if (evt.dragging) {
-      return;
+  mapMouseMove = map.on('pointermove', (ev) => {  // 给地图添加一个鼠标移动事件
+    let helpMsg = '点击开始测量'    // 默认开始的操作提示文本
+    if (feature) {  // feature 是全局的，判断有没有点击鼠标绘制过
+      helpMsg = '双击结束测量'    // 如果之前点击绘制了就显示双击结束
     }
-    /** @type {string} */
-    let helpMsg = 'Click to start drawing';
+    helpTooltipElement.innerHTML = helpMsg;   // 设置dom的提示文字
+    helpTooltip.setPosition(ev.coordinate);  // 设置位置跟着鼠标走
+    helpTooltipElement.classList.remove('hidden')  // 让他显示出来
+  })
+  createHelpTooltip()   // 创建那个helpTooltipElement方法
+  map.addLayer(layer)  // 把图层添加到地图
+}
 
-    if (sketch) {
-      const geom = sketch.getGeometry();
-      if (geom instanceof Polygon) {
-        helpMsg = continuePolygonMsg;
-      } else if (geom instanceof LineString) {
-        helpMsg = continueLineMsg;
-      }
-    }
-
-    helpTooltipElement.innerHTML = helpMsg;
-    helpTooltip.setPosition(evt.coordinate);
-
-    helpTooltipElement.classList.remove('hidden');
-  };
-
-  const map = new Map({
-    layers: [raster, vector],
-    target: mapElement.value,
-    view: new View({
-      center: [113.267252, 23.137949],
-      projection: 'EPSG:4326',
-      zoom: 15,
-    }),
+function createHelpTooltip() {
+  if (helpTooltipElement) {
+    helpTooltipElement.parentNode.removeChild(helpTooltipElement);
+  }
+  helpTooltipElement = document.createElement('div');
+  helpTooltipElement.className = 'ol-tooltip hidden';
+  helpTooltip = new Overlay({
+    element: helpTooltipElement,
+    offset: [15, 0],
+    positioning: 'center-left',
   });
+  map.addOverlay(helpTooltip);
+}
 
-  map.on('pointermove', pointerMoveHandler);
 
-  map.getViewport().addEventListener('mouseout', function () {
-    helpTooltipElement.classList.add('hidden');
-  });
-
-  const typeSelect = document.getElementById('type');
-
-  let draw; // global so we can remove it later
-
-  /**
-   * Format length output.
-   * @param {LineString} line The line.
-   * @return {string} The formatted length.
-   */
-  const formatLength = function (line) {
-    const length = getLength(line);
-    let output;
-    if (length > 100) {
-      output = Math.round((length / 1000) * 100) / 100 + ' ' + 'km';
-    } else {
-      output = Math.round(length * 100) / 100 + ' ' + 'm';
-    }
-    return output;
-  };
-
-  /**
-   * Format area output.
-   * @param {Polygon} polygon The polygon.
-   * @return {string} Formatted area.
-   */
-  const formatArea = function (polygon) {
-    const area = getArea(polygon);
-    let output;
-    if (area > 10000) {
-      output = Math.round((area / 1000000) * 100) / 100 + ' ' + 'km<sup>2</sup>';
-    } else {
-      output = Math.round(area * 100) / 100 + ' ' + 'm<sup>2</sup>';
-    }
-    return output;
-  };
-
-  function addInteraction() {
-    const type = typeSelect.value == 'area' ? 'Polygon' : 'LineString';
-    draw = new Draw({
-      source: source,
-      type: type,
-      style: new Style({
-        fill: new Fill({
-          color: 'rgba(255, 255, 255, 0.2)',
-        }),
-        stroke: new Stroke({
-          color: 'rgba(0, 0, 0, 0.5)',
-          lineDash: [10, 10],
-          width: 2,
-        }),
-        image: new CircleStyle({
-          radius: 5,
-          stroke: new Stroke({
-            color: 'rgba(0, 0, 0, 0.7)',
-          }),
-          fill: new Fill({
-            color: 'rgba(255, 255, 255, 0.2)',
-          }),
-        }),
-      }),
-    });
-    map.addInteraction(draw);
-
-    createMeasureTooltip();
-    createHelpTooltip();
-
-    let listener;
-    draw.on('drawstart', function (evt) {
-      // set sketch
-      sketch = evt.feature;
-
-      /** @type {import("../src/ol/coordinate.js").Coordinate|undefined} */
-      let tooltipCoord = evt.coordinate;
-
-      listener = sketch.getGeometry().on('change', function (evt) {
-        const geom = evt.target;
-        let output;
-        if (geom instanceof Polygon) {
-          output = formatArea(geom);
-          tooltipCoord = geom.getInteriorPoint().getCoordinates();
-        } else if (geom instanceof LineString) {
-          output = formatLength(geom);
-          tooltipCoord = geom.getLastCoordinate();
-        }
-        measureTooltipElement.innerHTML = output;
-        measureTooltip.setPosition(tooltipCoord);
-      });
-    });
-
-    draw.on('drawend', function () {
-      measureTooltipElement.className = 'ol-tooltip ol-tooltip-static';
-      measureTooltip.setOffset([0, -7]);
-      // unset sketch
-      sketch = null;
-      // unset tooltip so that a new one can be created
-      measureTooltipElement = null;
-      createMeasureTooltip();
-      unByKey(listener);
-    });
-  }
-
-  /**
-   * Creates a new help tooltip
-   */
-  function createHelpTooltip() {
-    if (helpTooltipElement) {
-      helpTooltipElement.parentNode.removeChild(helpTooltipElement);
-    }
-    helpTooltipElement = document.createElement('div');
-    helpTooltipElement.className = 'ol-tooltip hidden';
-    helpTooltip = new Overlay({
-      element: helpTooltipElement,
-      offset: [15, 0],
-      positioning: 'center-left',
-    });
-    map.addOverlay(helpTooltip);
-  }
-
-  /**
-   * Creates a new measure tooltip
-   */
-  function createMeasureTooltip() {
-    if (measureTooltipElement) {
-      measureTooltipElement.parentNode.removeChild(measureTooltipElement);
-    }
-    measureTooltipElement = document.createElement('div');
-    measureTooltipElement.className = 'ol-tooltip ol-tooltip-measure';
-    measureTooltip = new Overlay({
-      element: measureTooltipElement,
-      offset: [0, -15],
-      positioning: 'bottom-center',
-      stopEvent: false,
-      insertFirst: false,
-    });
-    map.addOverlay(measureTooltip);
-  }
-
-  /**
-   * Let user change the geometry type.
-   */
-  typeSelect.onchange = function () {
-    map.removeInteraction(draw);
-    addInteraction();
-  };
-
-  addInteraction();
-
-})
-
-// function initMap() {
-//   // 地图实例
-//   map = new Map({
-//     target: mapElement.value,                         // 对应页面里 id 为 map 的元素
-//     layers: [                              // 图层
-//       new Tile({                           // 使用瓦片渲染方法
-//         source: new OSM()                  // 图层数据源
-//       })
-//     ],
-//     view: new View({                       // 地图视图
-//       projection: "EPSG:4326",             // 坐标系，有EPSG:4326和EPSG:3857
-//       center: [114.064839, 22.548857],     // 深圳坐标
-//       minZoom: 5,                          // 地图缩放最小级别
-//       zoom: 15                             // 地图缩放级别（打开页面时默认级别）
-//     })
-//   })
-// }
 
 
 </script>
 
 <template>
   <div class="container">
-
     <div id="map" class="map" ref="mapElement"></div>
     <form class="otherForm">
       <label for="type">Measurement type &nbsp;</label>
@@ -319,30 +125,62 @@ onMounted(() => {
   </div>
 </template>
 
-<style scoped>
+<style>
 .map {
   height: 100%;
   width: 100%;
 
 }
 
-.otherForm {
-  position: fixed;
-  /* width: 13%; */
-  bottom: 2vh;
-  left: 2vw;
-  /* border: 1px solid red; */
-  display: flex;
-  flex-direction: column;
-}
-
-.otherForm * {
-  /* border: 1px solid pink; */
-  /* flex: 1; */
-}
-
 .container {
   height: 100vh;
   width: 100vw;
+}
+
+.otherForm {
+  position: fixed;
+  bottom: 1vh;
+  left: 2vw;
+}
+
+
+.ol-tooltip {
+  position: relative;
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: 4px;
+  color: white;
+  padding: 4px 8px;
+  opacity: 0.7;
+  white-space: nowrap;
+  font-size: 12px;
+  cursor: default;
+  user-select: none;
+}
+
+.ol-tooltip-measure {
+  opacity: 1;
+  font-weight: bold;
+}
+
+.ol-tooltip-static {
+  background-color: #ffcc33;
+  color: black;
+  border: 1px solid white;
+}
+
+.ol-tooltip-measure:before,
+.ol-tooltip-static:before {
+  border-top: 6px solid rgba(0, 0, 0, 0.5);
+  border-right: 6px solid transparent;
+  border-left: 6px solid transparent;
+  content: "";
+  position: absolute;
+  bottom: -6px;
+  margin-left: -7px;
+  left: 50%;
+}
+
+.ol-tooltip-static:before {
+  border-top-color: #ffcc33;
 }
 </style>
